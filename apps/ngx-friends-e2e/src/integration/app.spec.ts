48e7a924dfa6a5eb0187bee83c4766250c99115e
Ted Weatherly <tw3coder@gmail.com>
2021-03-06 15:13:25 -0600

    remove nx content on page

--- apps/ngx-friends-e2e/src/integration/app.spec.ts
+++ apps/ngx-friends-e2e/src/integration/app.spec.ts
@@ -4,9 +4,6 @@ describe('ngx-friends', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
     // Function helper example, see `../support/app.po.ts` file
     getGreeting().contains('Welcome to ngx-friends!');
   });
