91154f5825da60c5af1817bd7d6a4e62e97ded84
Ted Weatherly <tw3coder@gmail.com>
2021-03-06 15:37:17 -0600

    chore: add SharedModule

--- apps/ngx-friends/src/app/app.module.ts
+++ apps/ngx-friends/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    CoreModule
+    CoreModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ apps/ngx-friends/src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    CommonModule,
+    BrowserAnimationsModule
+  ]
+})
+export class SharedModule {
+}
