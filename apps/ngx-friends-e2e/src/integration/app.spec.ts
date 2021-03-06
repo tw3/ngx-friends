f620bc3c050c88bb8a09b04870c7273cc038c804
Ted Weatherly <tw3coder@gmail.com>
2021-03-06 17:52:17 -0600

    feat: show dashboard header in mat-toolbar, set header title to "NGX Friends"

--- apps/ngx-friends-e2e/src/integration/app.spec.ts
+++ apps/ngx-friends-e2e/src/integration/app.spec.ts
@@ -6,7 +6,7 @@ describe('ngx-friends', () => {
 
     it('should display welcome message', () => {
       // Function helper example, see `../support/app.po.ts` file
-      getGreeting().contains('Welcome to ngx-friends!');
+      getGreeting().contains('NGX Friends');
     });
   });
 
