cb420fcd23df6c7e5c5fc5e076af095c51b31e87
Ted Weatherly <tw3coder@gmail.com>
2021-03-06 17:31:14 -0600

    test: add e2e to verify 404 page

--- apps/ngx-friends-e2e/src/integration/app.spec.ts
+++ apps/ngx-friends-e2e/src/integration/app.spec.ts
@@ -1,10 +1,19 @@
 import { getGreeting } from '../support/app.po';
 
 describe('ngx-friends', () => {
-  beforeEach(() => cy.visit('/'));
+  describe('Valid pages', () => {
+    beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to ngx-friends!');
+    it('should display welcome message', () => {
+      // Function helper example, see `../support/app.po.ts` file
+      getGreeting().contains('Welcome to ngx-friends!');
+    });
+  });
+
+  describe('Invalid pages', () => {
+    it('should display 404 page', function() {
+      cy.visit('/invalid-route');
+      getGreeting().contains('404: page missing');
+    });
   });
 });
