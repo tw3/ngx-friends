7e1ea220a65056777562d3a67fb96d52239e181c
Ted Weatherly <tw3coder@gmail.com>
2021-03-06 20:12:52 -0600

    feat: disable submit button when form is invalid

--- apps/ngx-friends/src/app/dashboard/users/report/user-form/user-form.component.ts
+++ apps/ngx-friends/src/app/dashboard/users/report/user-form/user-form.component.ts
@@ -1,19 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'tw3-user-form',
   templateUrl: './user-form.component.html',
   styleUrls: ['./user-form.component.scss']
 })
-export class UserFormComponent implements OnInit {
+export class UserFormComponent implements OnInit, OnDestroy {
   formGroup: FormGroup;
+  isFormValid: boolean;
+
+  private readonly ngUnsubscribe: Subject<void> = new Subject<void>();
 
   constructor() {
   }
 
   ngOnInit(): void {
     this.buildForm();
+  }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 
   private buildForm(): void {
@@ -32,6 +42,22 @@ export class UserFormComponent implements OnInit {
         Validators.maxLength(3)
       ]))
     });
+
+    this.formGroup.statusChanges.pipe(
+      takeUntil(this.ngUnsubscribe)
+    ).subscribe(this.onFormGroupStatusChanged.bind(this));
+  }
+
+  private onFormGroupStatusChanged(status: string): void {
+    this.updateIsFormValid();
+  }
+
+  private updateIsFormValid(): void {
+    this.isFormValid = this.isControlValid(this.formGroup);
+  }
+
+  private isControlValid(c: AbstractControl): boolean {
+    return !c.invalid && !c.pending;
   }
 
 }
