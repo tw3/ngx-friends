eccec6570a16831190a252b618af1e2eb63b3df7
Ted Weatherly <tw3coder@gmail.com>
2021-03-07 14:59:43 -0600

    feat: add ngrx

--- apps/ngx-friends/src/app/core/core.module.ts
+++ apps/ngx-friends/src/app/core/core.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CoreMaterialModule } from './material/core-material.module';
 import { throwIfAlreadyLoaded } from '../shared/_util/throw-if-already-loaded';
 import { NotificationService } from './_services/notification-service/notification.service';
+import { CoreNgrxModule } from './ngrx/core-ngrx.module';
 
 @NgModule({
   imports: [
     BrowserModule,
-    CoreMaterialModule
+    CoreMaterialModule,
+    CoreNgrxModule
   ],
   providers: [
     NotificationService
---
+++ apps/ngx-friends/src/app/core/ngrx/core-ngrx.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { environment } from '../../../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+@NgModule({
+  imports: [
+    StoreModule.forRoot(
+      {},
+      {
+        metaReducers: !environment.production ? [] : [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true
+        }
+      }
+    ),
+    EffectsModule.forRoot([]),
+    !environment.production ? StoreDevtoolsModule.instrument() : []
+  ]
+})
+export class CoreNgrxModule {
+}
