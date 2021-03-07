d15ac18190e8c488fe1b86d94ea09fde5540ec16
Ted Weatherly <tw3coder@gmail.com>
2021-03-06 20:41:47 -0600

    fix: re-export CommonModule from SharedModule

--- apps/ngx-friends/src/app/shared/shared.module.ts
+++ apps/ngx-friends/src/app/shared/shared.module.ts
@@ -15,6 +15,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     NoContentComponent
   ],
   exports: [
+    CommonModule,
     CommonMaterialModule,
     FormsModule,
     ReactiveFormsModule,
