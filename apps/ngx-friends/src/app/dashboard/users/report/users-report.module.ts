cebb8b613eb69f4945629f17b9aeb017b67416f7
Ted Weatherly <tw3coder@gmail.com>
2021-03-06 18:33:02 -0600

    feat: add empty user form component

--- apps/ngx-friends/src/app/dashboard/users/report/users-report.module.ts
+++ apps/ngx-friends/src/app/dashboard/users/report/users-report.module.ts
@@ -3,9 +3,13 @@ import { NgModule } from '@angular/core';
 import { UsersReportRoutingModule } from './users-report-routing.module';
 import { UsersReportComponent } from './users-report.component';
 import { SharedModule } from '../../../shared/shared.module';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @NgModule({
-  declarations: [UsersReportComponent],
+  declarations: [
+    UsersReportComponent,
+    UserFormComponent
+  ],
   imports: [
     SharedModule,
     UsersReportRoutingModule
