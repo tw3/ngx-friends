ad7248135cbb35abfc44953d70294e647fad0bbe
Ted Weatherly <tw3coder@gmail.com>
2021-03-13 16:18:29 -0600

    test: improve unit test for UsersReportComponent

--- apps/ngx-friends/src/app/dashboard/users/report/users-report.component.spec.ts
+++ apps/ngx-friends/src/app/dashboard/users/report/users-report.component.spec.ts
@@ -1,8 +1,12 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UsersReportComponent } from './users-report.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
-describe('AppComponent', () => {
+describe('UsersReportComponent', () => {
+  let fixture: ComponentFixture<UsersReportComponent>;
+  let comp: UsersReportComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [],
@@ -11,9 +15,27 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UsersReportComponent);
+    comp = fixture.componentInstance;
+  });
+
   it('should create', () => {
-    const fixture = TestBed.createComponent(UsersReportComponent);
-    const comp = fixture.componentInstance;
     expect(comp).toBeTruthy();
+  });
+
+  it('should have the proper title', () => {
+    const h2Elem = fixture.debugElement.query(By.css('h2'));
+    expect(h2Elem.nativeElement.innerHTML.trim()).toBe('User Report');
+  });
+
+  it('should have user form subview', () => {
+    const elem = fixture.debugElement.query(By.css('ngf-users-report-user-form'));
+    expect(elem).toBeTruthy();
+  });
+
+  it('should have report graphs subview', () => {
+    const elem = fixture.debugElement.query(By.css('ngf-users-report-graphs'));
+    expect(elem).toBeTruthy();
   });
 });
