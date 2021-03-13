add552802b5b8a661179e8882a11470e1766bdfe
Ted Weatherly <tw3coder@gmail.com>
2021-03-13 00:33:49 -0600

    test: configure cypress-image-snapshot for ngx-friends, may use it later

--- apps/ngx-friends-e2e/src/plugins/index.js
+++ apps/ngx-friends-e2e/src/plugins/index.js
@@ -12,6 +12,7 @@
 // the project's config changing)
 
 const { preprocessTypescript } = require('@nrwl/cypress/plugins/preprocessor');
+const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin');
 
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
@@ -19,4 +20,7 @@ module.exports = (on, config) => {
 
   // Preprocess Typescript file using Nx helper
   on('file:preprocessor', preprocessTypescript(config));
+
+  // Add ability for visual testing using cypress-image-snapshot
+  addMatchImageSnapshotPlugin(on, config);
 };
