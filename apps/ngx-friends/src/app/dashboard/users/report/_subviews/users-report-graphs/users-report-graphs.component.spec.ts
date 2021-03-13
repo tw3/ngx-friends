f3a067662bb19c1ab972c6ab7886dc9dd1381ce1
Ted Weatherly <tw3coder@gmail.com>
2021-03-13 16:19:17 -0600

    test: improve unit test for UsersReportGraphsComponent

--- apps/ngx-friends/src/app/dashboard/users/report/_subviews/users-report-graphs/users-report-graphs.component.spec.ts
+++ apps/ngx-friends/src/app/dashboard/users/report/_subviews/users-report-graphs/users-report-graphs.component.spec.ts
@@ -3,6 +3,9 @@ import { UsersReportGraphsComponent } from './users-report-graphs.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { initialUsersState } from '../../../+state/users.reducer';
+import * as UsersActions from '../../../+state/users.actions';
+import { UserEntity } from '@ngf/shared-ui';
+import * as UsersSelectors from '../../../+state/users.selectors';
 
 describe('UsersReportGraphsComponent', () => {
   let store: MockStore;
@@ -28,6 +31,56 @@ describe('UsersReportGraphsComponent', () => {
 
   it('should create', () => {
     expect(comp).toBeTruthy();
+  });
+
+  describe('ngOnInit', () => {
+    it('should call initListenForUsersChange() and dispatch fetchUsersFromUserReports', () => {
+      const initListenForUsersChangeSpy = spyOn<any>(comp, 'initListenForUsersChange');
+      const dispatchSpy = spyOn(store, 'dispatch');
+
+      comp.ngOnInit();
+
+      expect(initListenForUsersChangeSpy).toHaveBeenCalledWith();
+      expect(dispatchSpy).toHaveBeenCalledWith(UsersActions.fetchUsersFromUserReports());
+    });
+  });
+
+  describe('initListenForUsersChange', () => {
+    it('should call handleUsersChange() when there is a users change', () => {
+      const handleUsersChangeSpy = spyOn<any>(comp, 'handleUsersChange');
+      const users: UserEntity[] = [{
+        name: 'Jessica', age: 37, weight: 166, friendNames: ['Jordan']
+      }];
+      UsersSelectors.selectUsers.setResult(users);
+
+      comp['initListenForUsersChange']();
+
+      expect(handleUsersChangeSpy).toHaveBeenCalledWith(users);
+    });
+  });
+
+  describe('handleUsersChange', () => {
+    it('should convert users into results and dispatch fetchFriendsGraphFromUserReports', () => {
+      const users: UserEntity[] = [{
+        name: 'Jessica', age: 37, weight: 166, friendNames: ['Jordan']
+      }];
+      const dispatchSpy = spyOn(store, 'dispatch');
+
+      comp['handleUsersChange'](users);
+
+      expect(comp.userAgeResults).toEqual([{ name: 'Jessica', value: 37 }]);
+      expect(comp.userWeightResults).toEqual([{ name: 'Jessica', value: 166 }]);
+      expect(comp.ageWeightResults).toEqual([{
+        name: 'Jessica',
+        series: [{
+          name: '',
+          x: 37,
+          y: 166,
+          r: 1
+        }]
+      }]);
+      expect(dispatchSpy).toHaveBeenCalledWith(UsersActions.fetchFriendsGraphFromUserReports());
+    });
   });
 
   // https://ngrx.io/guide/store/testing
